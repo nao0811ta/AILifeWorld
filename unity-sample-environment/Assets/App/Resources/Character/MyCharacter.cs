33bdfe8acfa49367da292c81d938e2c5d1babd80
masaru kanai <masaru.kanai@majimade.jp>
2016-10-09 12:12:09 +0900

    fixed character collision size

--- unity-sample-environment/Assets/App/Resources/Character/MyCharacter.cs
+++ unity-sample-environment/Assets/App/Resources/Character/MyCharacter.cs
@@ -15,8 +15,14 @@ public class MyCharacter : MonoBehaviour
         int yy = _data._y;
         int zz = _data._z;
 
+        var anchor = new GameObject("anchor");
+        anchor.transform.parent = this.transform;
+        anchor.transform.localScale = Vector3.one;
+        anchor.transform.localPosition = new Vector3(0, -0.45f, -0.25f);
+        anchor.transform.localRotation = Quaternion.identity;
+
         var root = new GameObject("root");
-        root.transform.parent = this.transform;
+        root.transform.parent = anchor.transform;
         for (int x = 0; x < xx; ++x)
         {
             for (int y = 0; y < yy; ++y)
@@ -32,8 +38,9 @@ public class MyCharacter : MonoBehaviour
                 }
             }
         }
-        root.transform.localScale = Vector3.one * 0.05f;
+        root.transform.localScale = Vector3.one * 0.1f;
         root.transform.localPosition = new Vector3(0, 0.5f, 0);
+        root.transform.localRotation = Quaternion.Euler(0, 180, 0);
     }
 
     void Start()
